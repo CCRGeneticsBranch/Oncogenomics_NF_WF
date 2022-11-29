13e6f7dfcb1f3c9ce216583903d7eb8fcda5056f
kopardev <vishal.koparde@nih.gov>
2022-11-29 14:40:55 -0500

    moving .nextflow to resultsDir

--- nf.sh
+++ nf.sh
@@ -5,14 +5,15 @@ set -e
 # load singularity and nextflow modules
 module load singularity nextflow
 
-# set .nextflow dir ... dont want this to go to $HOME/.nextflow
-export NXF_HOME="/data/khanlab3/kopardevn/.nextflow"
 
 # set workDir ... by default it goes to `pwd`/work
 # this can also be set using "workDir" in nextflow.config
 # export NXF_WORK="/data/khanlab3/kopardevn/AWS_MVP_test/work"
 export OUTDIR="/data/khanlab3/kopardevn/AWS_MVP_test"
-export OUTTAG="2" # workDir will be $OUTDIR/work.$OUTTAG and resultsDir will be $OUTDIR/results.$OUTTAG and singularity cache is set to $OUTDIR/.singularity
+export OUTTAG="3" # workDir will be $OUTDIR/work.$OUTTAG and resultsDir will be $OUTDIR/results.$OUTTAG and singularity cache is set to $OUTDIR/.singularity
+
+# set .nextflow dir ... dont want this to go to $HOME/.nextflow
+export NXF_HOME="$OUTDIR/results.$OUTTAG/.nextflow"
 
 printenv|grep NXF
 
