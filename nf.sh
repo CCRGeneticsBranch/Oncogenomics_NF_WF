e8ee1e8571a18dac41da96d79cc309d665f00877
kopardev <vishal.koparde@nih.gov>
2022-11-30 14:09:46 -0500

    separating configs for reuse

--- nf.sh
+++ nf.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# list of profiles
+# biowulf_test_run_local -> get interactive node and run there
+# biowulf_test_run_slurm -> get interactive node and submit jobs to slurm
+# 
+
+PROFILE = "biowulf_test_run_local"
+
 set -e
 
 SCRIPT_NAME="$0"
@@ -17,7 +24,7 @@ module load singularity nextflow
 # this can also be set using "workDir" in nextflow.config
 # export NXF_WORK="/data/khanlab2/kopardevn/AWS_MVP_test/work"
 export OUTDIR="/data/khanlab3/kopardevn/AWS_MVP_test"
-export OUTTAG="5" # workDir will be $OUTDIR/work.$OUTTAG and resultsDir will be $OUTDIR/results.$OUTTAG and singularity cache is set to $OUTDIR/.singularity
+export OUTTAG="6" # workDir will be $OUTDIR/work.$OUTTAG and resultsDir will be $OUTDIR/results.$OUTTAG and singularity cache is set to $OUTDIR/.singularity
 export RESULTSDIR="$OUTDIR/results.$OUTTAG"
 export WORKDIR="$OUTDIR/work.$OUTTAG"
 
@@ -34,7 +41,7 @@ cd $RESULTSDIR
 nextflow \
 	-C $CONFIG_FILE \
 	run \
-	-profile test_biowulf \
+	-profile $PROFILE \
 	$WF_HOME/main.nf -resume \
 	-with-report $RESULTSDIR/report.html \
 	-with-trace \
